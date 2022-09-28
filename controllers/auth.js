9214f795f13c2720d0e64b16df1ab4a272cacf22
sylva <sylvaejike@gmail.com>
2022-09-28 14:16:42 +0100

    omo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,57 @@
-const User = require("../models/user")
-const {registerValidation} = require("../utils/validation")
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { registerValidation, loginValidation } = require("../utils/validation");
 
 
 exports.register = async (req, res) => {
-    const validation = registerValidation(req.body)
-    try {
-      if (validation["error"]) {
-        res.send(validation["error"].details[0].message);
-      } else {
-        const user = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        });
-        const savedUser = await user.save();
-        res.status(201).send(savedUser);
-      }
-    } catch (err) {
-      res.send(400).send(err);
+  const validation = registerValidation(req.body);
+  try {
+    if (validation["error"]) {
+      res.send(validation["error"].details[0].message);
+    } 
+
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send("Email already exists.");
+
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    
+    const savedUser = await user.save();
+    res.status(201).send(savedUser);
+    
+  } catch (err) {
+    res.send(400).send(err);
+  }
+};
+
+
+
+exports.login = async (req, res) => {
+  const validation = loginValidation(req.body)
+  try {
+    if (validation["error"]) {
+      res.send(validation["error"].details[0].message);
     }
+    
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Email not found.");
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send("Invalid Password.")
+    
+    // create jwt token
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).send(token);
+  } catch (error) {
+    res.send(400).send(error);
+  }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { getPosts, createPost, getPost, updatePost, deletePost } = require("../controllers/posts")
+const veryAuth = require("../utils/verifyauth")
 
 const router = express.Router();
 
-router.get("/", getPosts);
-router.post("/", createPost);
-router.get("/:id", getPost);
-router.put("/:id", updatePost);
-router.delete("/:id", deletePost);
+router.get("/", veryAuth, getPosts);
+router.post("/", veryAuth, createPost);
+router.get("/:id", veryAuth, getPost);
+router.put("/:id", veryAuth, updatePost);
+router.delete("/:id", veryAuth, deletePost);
 
 
 module.exports = router;
